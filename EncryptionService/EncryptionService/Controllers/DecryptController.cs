23f5b671dc758df4e505e349eed2469f0bf924dd
Dag König <dagk@microsoft.com>
2018-12-18 11:23:13 +0100

    endpoints created

---
+++ EncryptionService/EncryptionService/Controllers/DecryptController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EncryptionService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DecryptController : ControllerBase
+    {
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get(string data, string key)
+        {
+            return "hello";
+        }
+    }
+}
